e8a6d1f2a8213fd33bce452e9965cc8172dbbcaf
SerhiiKutovyi <wagenrex@gmail.com>
2025-11-03 22:52:52 +0200

    I

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import CafeInfo from "../Cafeinfo/Cafeinfo";
+import CafeInfo from "../CafeInfo/CafeInfo";
 import Notification from "../Notification/Notification";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
